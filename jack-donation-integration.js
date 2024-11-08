9477dd94bb8a5162f9991ec6b1a86e858e0170df
Nicholas Defelice <nick.d@threesixtyeight.is>
2024-11-08 16:44:08 -0600

    Add multiple recipients to eCards

--- jack-donation-integration.js
+++ jack-donation-integration.js
@@ -127,7 +127,7 @@ $(document).ready(function() {
             message = "Invalid CVD data (not 3-4 digits).";
             break;
           default:
-            message = "Error saving credit card, please contact us hello@jack.org";
+            message = "Error saving credit card, please contact us donate@jack.org";
             window.currentDonationForm.find('[data-donate="complete-button"]').prop('disabled', false);
             window.currentDonationForm.find('[data-donate="complete-button"] .btn_main_text').text('Donate');
             toggleProcessing(false);
@@ -303,19 +303,30 @@ $(document).ready(function() {
               // Check if ecard is selected by checking if value is 'e-card'
               if ($form.find('[data-donate="ecard-selection"] input:checked').val() === 'e-card') {
                   // Get the selected ecard design value
-                  const selectedTemplateId = parseInt($form.find('[data-donate="ecard-design"] input:checked').val(), 10);
+                  let selectedTemplateId = parseInt($form.find('[data-donate="ecard-design"] input:checked').val(), 10);
+                  if (isNaN(selectedTemplateId)) {
+                      selectedTemplateId = parseInt($form.find('[data-donate="ecard-design"] input:visible:first').val(), 10);
+                  }
                   
                   tributeObject.eCard = {
                       templateId: selectedTemplateId,
                       message: $form.find('[data-donate="message"]').val().trim(), 
                       deliveryDate: $form.find('[data-donate="date"]').val().trim(),
-                      recipients: [
-                          {
-                              firstName: $form.find('[data-donate="recipient-first-name"]').val().trim(),
-                              lastName: $form.find('[data-donate="recipient-last-name"]').val().trim() || "",
-                              email: $form.find('[data-donate="recipient-email"]').val().trim()
+                      recipients: $form.find('[data-donate="add"]').map(function() {
+                          const $recipientBlock = $(this);
+                          const email = $recipientBlock.find('[data-donate="recipient-email"]').val().trim();
+                          const firstName = $recipientBlock.find('[data-donate="recipient-first-name"]').val().trim();
+                          const lastName = $recipientBlock.find('[data-donate="recipient-last-name"]').val().trim();
+                          
+                          // Only include recipients that have at least an email address
+                          if (email && firstName) {
+                              return {
+                                  firstName: firstName,
+                                  lastName: lastName || "",
+                                  email: email
+                              };
                           }
-                      ]
+                      }).get() // Convert jQuery object to array and filter out undefined values
                   };
               }
 
